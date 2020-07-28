2d7246059eb2d7cd7980345ee1a66c0ce28b742d
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-07-28 22:02:10 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
+require 'pry'
+
 def unsafe?(speed)
-
-end
-
-
+  if speed < 40 || speed > 60
+    return true
+  else 
+    false
+  end
+end	
 
 def not_safe?(speed)
-	
-end
-	
+	speed < 40 || speed > 60 ? true : false 
+end	
 
 
